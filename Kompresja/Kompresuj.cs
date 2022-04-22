cdbaec4670b519dac8c5fa1d14a730bbc9e46a2d
Zabojaca <101509595+Zabojaca@users.noreply.github.com>
2022-04-22 22:16:52 +0200

    Dodano projekt Kompresja

---
+++ Kompresja/Kompresuj.cs
@@ -0,0 +1,21 @@
+﻿using P = Pudelko.Pudelko;
+using Pudelko;
+
+namespace Kompresja
+{
+    public static class Kompresja
+    {
+        public static P Kompresuj(P obj)
+        {            
+            double a = Math.Cbrt(obj.Objetosc);
+            return new P(a, a, a, UnitOfMeasure.meter);
+        }
+
+        public static void Main()
+        {
+            P p = new P(2, 8, 4);
+            var skompresowane = Kompresuj(p);
+            Console.WriteLine(skompresowane);
+        }
+    }
+}
